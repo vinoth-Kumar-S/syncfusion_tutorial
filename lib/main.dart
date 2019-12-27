eb6e64a4792241c439f3ce8c582a6832812d9018
vinoth-Kumar-S <vinothkumar1893@gmail.com>
2019-12-27 11:03:31 +0530

    Integrating Home page and Page Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,147 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Syncfusion Tutorial Videos'),
+      home: HomePage(),
+      routes: <String, WidgetBuilder>{
+        '/CardPage': (BuildContext context) => new CardPage(),
+        '/ContentPage': (BuildContext context) => new ContentPage()
+      }
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  //   final scaffoldkey=new GlobalKey<ScaffoldState>();
+  // final formkey=new GlobalKey<FormState>();
+    String _email;
+   String _password;
+  //  void performlogin(){
+  //   final snackbar=new SnackBar(
+  //     content: new Text("Email: $_email, Password: $_password"),);
+  //     scaffoldkey.currentState.showSnackBar(snackbar);
+  // }
+   void _submit () {
+    // final form=formkey.currentState;
+    // if(form.validate()){
+    //   form.save();
+      
+    //   performlogin();
+    // } 
+    Navigator.of(context).pushNamed('/CardPage');   
+  }
+  
+    final emailField = TextFormField(
+          obscureText: false,
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Email",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+          validator: (val)=> !val.contains('@')?'Invalid Email':null, 
+          onSaved: (val)=>_email=val,      
+        );
+        final passwordField = TextFormField(
+          obscureText: true,
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Password",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+          validator: (val)=> val.length<6?'Password Too Short':null,
+          onSaved: (val)=>_password=val,
+        );
+        final loginButon = Material(
+          elevation: 5.0,
+          borderRadius: BorderRadius.circular(30.0),
+          color: Color(0xff01A0C7),
+          child: MaterialButton(
+            minWidth: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),            
+            child: Text("Login",
+                textAlign: TextAlign.center,
+                style: style.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            onPressed:_submit,
+          ),
+        );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Syncfusion Tutorial'),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+            SizedBox(
+                      height: 155.0,
+                      child: Image.asset(
+                        "assets/Logo.png",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    loginButon,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+          ]
+          
+          )
+        ),
+      ),
+    );
+  }
+}
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class CardPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Home'),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+            new IconButton(
+              icon: new Icon(Icons.favorite, color: Colors.red ),
+              onPressed: null,
+            ),
+            new Text('Home')
+          ]
+          )
+        ),
+      ),
+    );
+  }
+}
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class ContentPage extends StatefulWidget {
+  ContentPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ContentPageState createState() => _ContentPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _ContentPageState extends State<ContentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -93,15 +199,21 @@ class _MyHomePageState extends State<MyHomePage> {
             const ListTile(
               leading: Icon(Icons.album),
               title: Text('Angular'),
+              dense: true,
             ),
+            SizedBox(height: 10),
             const ListTile(
               //leading: Icon(Icon()),
               title: Text('Flutter'),
+              dense: true,
             ),
+            SizedBox(height: 10),
             const ListTile(
               //leading: Icon(Icon.album),
               title: Text('React'),
-            )
+              dense: true,
+            ),
+            SizedBox(height: 10),
           ],
         ),
       ),
